32dd8c557b4b166e4cdd812fe19cd578e0b5c8e6
theentropic <edabramson@gmail.com>
2019-07-20 10:13:16 -0400

    Add checks and grab tags based on filter

--- GitTagLoader/index.ts
+++ GitTagLoader/index.ts
@@ -4,9 +4,15 @@ import shell = require('shelljs');
 async function run() {
     try {
         const lines: string = tl.getInput('lines', true);
+        const prefix: string = tl.getInput('prefix', false) || '';
         let filter: string = tl.getInput('filter', true);
+        
+        if (!parseInt(lines, 10)) {
+            tl.setResult(tl.TaskResult.Failed, `lines parameter must be a valid number`, true);
+            return;
+        }
 
-        if (shell.which('git')) {
+        if (!shell.which('git')) {
             tl.setResult(tl.TaskResult.Failed, `git is required to run this script`, true);
             return;
         }
@@ -20,8 +26,13 @@ async function run() {
             tl.setResult(tl.TaskResult.Failed, `Specified tag ${filter} not found`, true);
             return;
         }
-
         
+        tl.setVariable(`${prefix}Tag.Label`, tag);
+        console.log(`Found tag`, tag);
+
+        const annot = shell.exec(`git tag -n${lines} "${tag}"`).stdout;
+        tl.setVariable(`${prefix}Tag.Annotation`, annot);
+        tl.setResult(tl.TaskResult.Succeeded, `Finished processing`, true);
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
